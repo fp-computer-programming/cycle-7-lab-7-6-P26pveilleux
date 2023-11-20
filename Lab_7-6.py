1c0f3609cc271adebd76c675df40a028cc3a9c9e
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-11-20 09:18:58 -0500

    done

---
+++ Lab_7-6.py
@@ -0,0 +1,17 @@
+
+#Create a Python file named lab_7-6.py
+
+#*** You must write a comment for every chunk of code you write from now on along with your author tag***
+
+#Create a 2 separate functions within the same document. 
+def grams_to_kg(grams):
+    return gram * 1000
+def m_times_m(m):
+    return m*m
+
+#Create a 3rd function which requires both the first two functions within it
+
+def bmi (grams,m):
+    return grams_to_kg(grams)/m_times_m(m)
+#Create 4 test cases for your 3rd function. 
+
